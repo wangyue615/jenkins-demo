0fd64a400aa96992f5cbeae4d8332c862a524bdc
DiJun <xuewang@DiJundeMacBook-Pro.local>
2020-05-01 13:21:04 +0800

    init commit

---
+++ src/main/java/com/wangyue/learn/tools/jenkins/jenkinsdemo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.wangyue.learn.tools.jenkins.jenkinsdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
